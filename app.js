aa137ede066febe8f16cab28778dc2c5758bd53e
Estefanía Otálvaro Mora <estefaniaotalvarom@gmail.com>
2017-01-20 10:53:44 -0500

    Con controladores
    En los controladores esta el API (Insertar, modificar, Consultar y
    Eliminar) CRUD

--- app.js
+++ app.js
@@ -1,26 +1,10 @@
 var express = require("express"),
 	app = express(),
-
 	bodyParser = require("body-parser"),
 	methodOverride = require("method-override")
-
-	http = require("http"),
-	server = http.createServer(app);
-
 	mongoose = require('mongoose');
 
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
-app.use(methodOverride());
-
-var router = express.Router();
-
-router.get('/', function(req, res) {
-	res.send("Hello World!");
-});
-
-app.use(router);
-
+//Connection to DB
 mongoose.connect('mongodb://localhost/tvshows', function(err,res){
 	if(err){
 		console.log('ERROR: connecting to Database. ' + err);
@@ -29,3 +13,34 @@ mongoose.connect('mongodb://localhost/tvshows', function(err,res){
 		console.log("Node server running on http://localhost:3000");
 	});
 });
+
+//Middlewares
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(methodOverride());
+
+//Import Models and Controllers
+var models = require('./models/tvshow')(app,mongoose);
+var TVShowCtrl = require('./controllers/tvshows');
+
+// Example Route
+var router = express.Router();
+router.get('/', function(req, res) {
+  res.send("Hello world!");
+});
+app.use(router);
+
+//API routes
+var tvshows = express.Router();
+
+tvshows.route('/tvshows')
+	.get(TVShowCtrl.findAllTVShows)
+	.post(TVShowCtrl.addTVShow);
+
+tvshows.route('/tvshows/:id')
+	.get(TVShowCtrl.findById)
+	.put(TVShowCtrl.updateTVShow)
+	.delete(TVShowCtrl.deleteTVShow);
+
+app.use('/api', tvshows);
+
