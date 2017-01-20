8ecf443e2caa339f6fc39b246ae7ea143e984339
Estefanía Otálvaro Mora <estefaniaotalvarom@gmail.com>
2017-01-20 11:47:35 -0500

    Cambios en el proyecto

--- app.js
+++ app.js
@@ -1,26 +1,22 @@
-var express = require("express"),
-	app = express(),
-	bodyParser = require("body-parser"),
-	methodOverride = require("method-override")
-	mongoose = require('mongoose');
+var express         = require("express"),
+    app             = express(),
+    bodyParser      = require("body-parser"),
+    methodOverride  = require("method-override"),
+    mongoose        = require('mongoose');
 
-//Connection to DB
-mongoose.connect('mongodb://localhost/tvshows', function(err,res){
-	if(err){
-		console.log('ERROR: connecting to Database. ' + err);
-	}
-	app.listen(3000, function(){
-		console.log("Node server running on http://localhost:3000");
-	});
+// Connection to DB
+mongoose.connect('mongodb://localhost/tvshows', function(err, res) {
+  if(err) throw err;
+  console.log('Connected to Database');
 });
 
-//Middlewares
+// Middlewares
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(methodOverride());
 
-//Import Models and Controllers
-var models = require('./models/tvshow')(app,mongoose);
+// Import Models and controllers
+var models     = require('./models/tvshow')(app, mongoose);
 var TVShowCtrl = require('./controllers/tvshows');
 
 // Example Route
@@ -30,17 +26,21 @@ router.get('/', function(req, res) {
 });
 app.use(router);
 
-//API routes
+// API routes
 var tvshows = express.Router();
 
 tvshows.route('/tvshows')
-	.get(TVShowCtrl.findAllTVShows)
-	.post(TVShowCtrl.addTVShow);
+  .get(TVShowCtrl.findAllTVShows)
+  .post(TVShowCtrl.addTVShow);
 
 tvshows.route('/tvshows/:id')
-	.get(TVShowCtrl.findById)
-	.put(TVShowCtrl.updateTVShow)
-	.delete(TVShowCtrl.deleteTVShow);
+  .get(TVShowCtrl.findById)
+  .put(TVShowCtrl.updateTVShow)
+  .delete(TVShowCtrl.deleteTVShow);
 
 app.use('/api', tvshows);
 
+// Start server
+app.listen(3000, function() {
+  console.log("Node server running on http://localhost:3000");
+});
